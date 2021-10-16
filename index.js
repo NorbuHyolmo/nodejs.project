d8a5093dbe30c89c0c4426bf961d01f3caa684ba
Norbu Lama <norbuhyolmo7@gmail.com>
2021-10-16 19:47:13 +0545

    adding index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+
+//THE CALLBACKS ARE REQUEST AND RESPONSE
+//FIRST WE CREATED A SERVER USING THE CODE BELOW 
+//HTTP.CREATESERVER WHICH INCLUDES REQUEST AND RESPONSE CALLBACKS
+const server = http.createServer((request, response) =>{
+    //WE SENT 200 STATUS CODE 
+    //AND CONTENT TYPE AS HTML TEXT
+    response.writeHead(200, {'content-type': 'text/html'});
+    //RESPONSE.WRITE == BODY
+    response.write('<h1> Hello IIMS </h1>'); 
+    //RESPONSE.END MEANS END AND EXECUTE THE RESPONSES
+    response.end();
+})
+
+//NEED TO PROVIDE A LOCAL HOST TO RUN THE RESPONSE
+//3000 IS THE LOCAL HOST
+//CAN WRITE ANY LOCAL HOST CODE
+// server.listen(1000);  OR
+
+const port = 3000;
+server.listen(port,()=>{
+//USE ` TO SHOW ANY STRING DYANAMICALLY 
+//AND USE ${STRING} TO SHOW ANY STRING DYNAMICALLY
+    console.log(`Server is running at port: ${port}`);
+});
