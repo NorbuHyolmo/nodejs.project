37111ea5c2823ad111ee0283e92c3b3615fe2204
Norbu Lama <norbuhyolmo7@gmail.com>
2021-10-18 15:51:39 +0545

    installed packages and done get and post

--- README.md
+++ README.md
@@ -15,4 +15,13 @@ let user = [{
 }
 ]
 Routes    |   Request Method   |    Description 
-user 1          Post                Create a new User
+/user            Post                Create a new User
+/user            Get                 Get all user info
+/user/:id        Put/Patch           Update user info
+/user/:id        Get                 Retrieve user info
+/user/:id        Delete              Delete using Uid
+
+
+-PUT/ PATCH 
+- ALL INFORMATION CHANGE { USE PUT }
+- JUST CHANGE CERTAIN INFORMATION{ USE PATCH }
--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+require('dotenv').config()
+
+// WE NEED A FUNCION TO CALL GET, PATCH, ETC 
+// SO, FIRST WE SET EXPRESS FUNCTION IN A STRING APP
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get("/", function(request,response){
-    response.send("<h1>Hello World</h1>");
-})
-//OR
-app.get("/about",(request, response)=>{
-    response.send("This is the about page");
-});
 
-app.get("/profile", function(request,response){
-    //IF WE WANT TO ADD CUSTOMIZED STATUS CODE
-    response.status(200).json({
-        "Name" : "Norbu Lama",
-        "Location" : "Kathmandu"
-    }); 
+let users = []; //id, name, email, address
+
+// TO ACCESS THE MIDDLEWARE 
+// OR ACCESS THE BODY-PARSER
+app.use(bodyParser.json());
+
+//CRUD
+//GET ALL USERS , REQUEST METHOD: GET
+app.get('/users',(request, response)=>{
+    response.json(users);
 })
-app.listen(3000);
+
+//CREATE NEW USER, REQUEST METHOD: POST
+app.post('/users',(request,response)=>{
+    users.push(request.body);
+    response.status(201).json(request.body);
+})
+
+//GET USERS BY ID, REQUEST METHOD: GET
+app.get('/users/:id',(req,res)=>{
+    //"req.params.id" replaces the value with the id given in the placeholder
+    // it also converts the value into string 
+    let user = users.find(user=> user.id ===parseInt(req.params.id));
+    if(!user)res.status(404).send('The user with the given ID was not found.');
+    res.json(user);
+})
+
+
+// '||' MEANS EITHER GIVEN PORT OR THE 3000
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`this server is running at port: ${port}`);
+});
